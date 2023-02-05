7d98163c4a5086ca058c82bd43b5c4ce5c9b72a2
Zhyldyz <jargynbaeva@gmail.com>
2023-02-05 10:20:02 +0100

    API for News done

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
+import moment from 'moment';
 
-const News = () => {
+import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+
+const { Text, Title } = Typography;
+const { Option } = Select;
+const demoImage = "http://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg";
+
+const News = ({simplified}) => {
+  const {data: cryptoNews} = useGetCryptoNewsQuery({newsCategory: "Cryptocurrency", count: simplified ? 6 : 12})
+  console.log(cryptoNews)
+  if(!cryptoNews?.yahoo) return 'Loading ...'
   return (
-    <div>News</div>
+    <>
+    <Row gutter={[24, 24]}>
+      {cryptoNews.yahoo.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className='news-card'>
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className='news-image-container'>
+                <Title className='news-title' level={4}>{news.title}</Title>
+                <img src={demoImage} alt="demoImage" />
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+    </>
   )
 }
 
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+ 
+const cryptoNewsHeaders = {
+  "X-RapidAPI-Key": "53f8b703fcmshd420b3ebcbd5c40p1cea85jsn3e5e4f98f23b",
+  "X-RapidAPI-Host": "crypto-news16.p.rapidapi.com",
+};
+  
+const baseUrl = "https://crypto-news16.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: () => createRequest(`/news/all`),
+    }),
+  }),
+});
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
